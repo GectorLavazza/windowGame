2e1628c2a0507673be171d19824ab18e05565e7f
pavlen5kiy <wolkpavel51@gmail.com>
2024-11-02 15:01:21 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import time
+
+import pygame
+
+from screen import Screen
+
+pygame.init()
+clock = pygame.time.Clock()
+fps = 60
+screen = Screen((600, 400))
+
+last_time = time.time()
+
+running = True
+
+particles = pygame.sprite.Group()
+
+while running:
+
+    dt = time.time() - last_time
+    dt *= 60
+    last_time = time.time()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                screen.dy = -1
+            if event.key == pygame.K_s:
+                screen.dy = 1
+            if event.key == pygame.K_a:
+                screen.dx = -1
+            if event.key == pygame.K_d:
+                screen.dx = 1
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                screen.dy = 0
+            if event.key == pygame.K_s:
+                screen.dy = 0
+            if event.key == pygame.K_a:
+                screen.dx = 0
+            if event.key == pygame.K_d:
+                screen.dx = 0
+
+    screen.update(dt)
+
+    pygame.display.update()
+    clock.tick(fps)
+
+pygame.quit()
---
+++ screen.py
@@ -0,0 +1,39 @@
+import pygame
+
+
+class Screen:
+    def __init__(self, size):
+        self.set_screen(size)
+
+        self.dx, self.dy = 0, 0
+        self.velocity = pygame.Vector2(0, 0)
+
+        self.speed = 10
+
+    def set_screen(self, size):
+        self.screen = pygame.display.set_mode(size, pygame.DOUBLEBUF)
+        self.screen_rect = (0, 0, size[0], size[1])
+        self.size = size
+
+    def update(self, dt):
+        pygame.display.set_caption(str(tuple(map(round, self.size))))
+        self.screen.fill(pygame.Color('black'))
+        self.move(dt)
+
+    def move(self, dt):
+        input_direction = pygame.Vector2(self.dx, self.dy)
+
+        if input_direction.length() > 0:
+            input_direction = input_direction.normalize()
+
+        self.velocity.x = input_direction.x * dt * self.speed
+        self.velocity.y = input_direction.y * dt * self.speed
+
+        if self.velocity.length() > self.speed:
+            self.velocity = self.velocity.normalize() * self.speed
+
+        new_size = (self.size[0] + self.velocity.x,
+                    self.size[1] + self.velocity.y)
+
+        if all(map(lambda n: n >= 200, new_size)):
+            self.set_screen(new_size)
